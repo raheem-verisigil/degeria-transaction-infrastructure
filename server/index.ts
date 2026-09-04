6193632fdc10bd92d32b7b89d13fed21102add55
Raheem Larry Babatunde <raheem@verisigilai.com>
2026-09-04 11:30:38 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -25,7 +25,7 @@ async function startServer() {
 
   const port = process.env.PORT || 3000;
 
-  server.listen(port, () => {
+ server.listen(Number(port), "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${port}/`);
   });
 }
